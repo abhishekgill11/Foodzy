0c0a4c4fc4063380d11340f5991132305a1c076e
Abhishek <1rn19ec002.abhishek@gmail.com>
2023-03-21 02:31:31 +0530

    adding more functionalities

--- backend/Routes/DisplayData.js
+++ backend/Routes/DisplayData.js
@@ -1,13 +1,15 @@
 const express = require('express')
 const router = express.Router()
-
-router.post('/foodData', (req,res) => {
+router.post('/foodData', async (req, res) => {
     try {
-        res.send([global.food_menu, global.foodCategory])
-        console.log(global.food_menu)
-    } catch(error) {
-        console.error(error.message);
+        // console.log( JSON.stringify(global.foodData))
+        // const userId = req.user.id;
+        // await database.listCollections({name:"food_items"}).find({});
+        res.send([global.foodData, global.foodCategory])
+    } catch (error) {
+        console.error(error.message)
         res.send("Server Error")
+
     }
 })
 module.exports = router;
--- backend/index.js
+++ backend/index.js
@@ -26,6 +26,7 @@ app.get('/', (req, res) => {
 app.use(express.json())
 app.use('/api/', require("./Routes/CreateUser"));
 app.use('/api/', require("./Routes/DisplayData"));
+app.use('/api/', require("./Routes/OrderData"));
 app.listen(port, () => {
     console.log(`Connected to Mongodb ${port}`)
 })
